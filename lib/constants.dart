d124fdfc6260f3478acf40482925c23cd35c195c
Shreya Banga <shreya.banga@gmail.com>
2021-12-26 14:10:42 -0500

    test

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@
 // can keep adding to this list to fetch any article, ui updates automatically
 const articles = [
   'The_Cat_in_the_Hat',
-  'The_Cat_in_the_Hat',
+  'Green_Eggs_and_Ham',
   'Green_Eggs_and_Ham',
   
 ];
